3a35c03f49ebf8638b5fdf73ee583b4605cd7c29
Ivan Fraixedes <ifcdev@gmail.com>
2013-08-31 15:27:13 +0100

    return empty object rather than empty array to make more sense with the action will return in the future

--- server/controllers/app/actions/showSpeaker.js
+++ server/controllers/app/actions/showSpeaker.js
@@ -10,11 +10,12 @@ var defLogger = settings.appLoggers.getWinstonLogger();
  * @param req
  * @param res
  * @param next
+ * @para post
  */
 module.exports = function showSpeaker(req, res, next, post) {
 
 	// No speakers at the moment
-	res.json(200, []);
+	res.json(200, {});
 	post(null, req, res);
 
 };
