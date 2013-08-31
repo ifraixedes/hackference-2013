0cdb4d216eb244d90d1810e243243aa3d3492cbd
Ivan Fraixedes <ifcdev@gmail.com>
2013-09-01 00:50:10 +0100

    Added behaviour to integrate the new services

--- server/app.js
+++ server/app.js
@@ -1,3 +1,6 @@
+
+
+
 module.exports = function (callback) {
 	'use strict';
 
@@ -12,6 +15,7 @@ module.exports = function (callback) {
 	var expressBootstrapper = require(settings.libsPath + '/ifc/util/expressBootstrapper');
 	var expressApp = settings.expressApp = express();
 	var httpServer = http.createServer(expressApp);
+	var appServices = require(settings.servicesPath);
 
 // Configuration variables
 	var appCnf = settings.configLoader('app');
@@ -41,10 +45,30 @@ module.exports = function (callback) {
 		}
 	}
 
+	appServices.start(function (err) {
 
-	/***** HTTP Server start *****/
-	httpServer.listen(appCnf.http.port);
-	callback(null, httpServer);
+		if (err) {
+			callback(err);
+			return;
+		}
+
+
+		httpServer.on('close', function () {
+
+			appServices.stop(function (err) {
+
+				if (err) {
+					console.error('Server closed with errors. Some service stop failed. Error details: ' +
+						err.message);
+				} else {
+					console.info('Server closed');
+				}
+			});
+		});
+
+		/***** HTTP Server start *****/
+		httpServer.listen(appCnf.http.port);
+		callback(null, httpServer);
+	});
+
 };
-
-
