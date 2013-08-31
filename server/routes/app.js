0a2588e7334d9415166814feea2282b2c35e0df3
Ivan Fraixedes <ifcdev@gmail.com>
2013-08-31 15:27:55 +0100

    Added route to show speaker and the test to test it

--- server/routes/app.js
+++ server/routes/app.js
@@ -7,27 +7,17 @@ var appCtrl = require(settings.ctrlsPath + '/app');
 
 
 module.exports = [
-//	{
-//		// We may use regular expressions (/\/messages\/([\d]*)/) but, express process the request
-//		// although the 'id' is not a number, assigning to req.params[0] an empty string, so better use
-//		// a middleware to parse it and it is not number response with error
-//		path: '/messages/:id',
-//		method: 'get',
-//		action: appCtrl.actions.getMessage,
-//		pre: appCtrl.middlewares.pre.checkMsgId,
-//		post: [
-//			genericCtrl.middlewares.post.errorTracker,
-//			genericCtrl.middlewares.post.ipTracker
-//		]
-//	},
-//	{
-//		path: '/messages',
-//		method: 'post',
-//		action: appCtrl.actions.storeMessage,
-//		pre: appCtrl.middlewares.pre.sanitiseMsgBody,
-//		post: [
-//			genericCtrl.middlewares.post.errorTracker,
-//			genericCtrl.middlewares.post.ipTracker
-//		]
-//	}
+	{
+		// We may use regular expressions (/\/messages\/([\d]*)/) but, express process the request
+		// although the 'id' is not a number, assigning to req.params[0] an empty string, so better use
+		// a middleware to parse it and it is not number response with error
+		path: '/show/speaker',
+		method: 'get',
+		action: appCtrl.actions.showSpeaker,
+		pre: appCtrl.middlewares.pre.parseSpeakerQuery,
+		post: [
+			genericCtrl.middlewares.post.errorTracker,
+			genericCtrl.middlewares.post.twilioNotifier
+		]
+	}
 ];
--- tests/server.js
+++ tests/server.js
@@ -24,14 +24,14 @@ describe('when server is up, ', function () {
 		httpServer.close();
 	});
 
-	describe('sending', function () {
+	describe('requesting', function () {
 
-		it('a new message the server should respond with new message id', function (done) {
+		it('show speaker return an empty object', function (done) {
 			var req = http.request({
 				host: '127.0.0.1',
 				port: '8010',
 				path: '/show/speaker',
-				method: 'post',
+				method: 'get',
 				headers: {
 					'Content-Type': 'application/json'
 				}
@@ -42,10 +42,8 @@ describe('when server is up, ', function () {
 
 					res.on('data', function (chunk) {
 						try {
-							var objId = JSON.parse(chunk);
-							objId.id.should.a('number');
-
-							newMsgId = objId.id;
+							var emptyObject = JSON.parse(chunk);
+							emptyObject.should.a('object');
 
 							done();
 						} catch (e) {
@@ -61,144 +59,9 @@ describe('when server is up, ', function () {
 				done(err);
 			});
 
-			req.write(JSON.stringify({message: 'test'}));
+			//req.write(JSON.stringify({message: 'test'}));
 			req.end();
 		});
-
-		it('a new message with a possilbe XSS should return 400 code and "fuck off"', function (done) {
-			var req = http.request({
-				host: '127.0.0.1',
-				port: '8010',
-				path: '/messages',
-				method: 'post',
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}, function (res) {
-				try {
-					res.should.have.status(400);
-					res.setEncoding('utf8');
-
-					res.on('data', function (chunk) {
-						try {
-							chunk.should.equal('fuck off');
-							done();
-						} catch (e) {
-							done(e);
-						}
-					});
-				} catch (e) {
-					done(e);
-				}
-			});
-
-			req.on('error', function (err) {
-				done(err);
-			});
-
-			req.write(JSON.stringify({message: '<script>alert()</script>'}));
-			req.end();
-
-		});
-	});
-
-	describe('getting', function () {
-
-		it('a existing message the server should respond with it', function (done) {
-			var req = http.request({
-				host: '127.0.0.1',
-				port: '8010',
-				path: '/messages/' + newMsgId,
-				method: 'get'
-			}, function (res) {
-				try {
-					res.should.have.status(200);
-					res.setEncoding('utf8');
-
-					res.on('data', function (chunk) {
-						try {
-							var objId = JSON.parse(chunk);
-							objId.should.eql({message: 'test'});
-							done();
-						} catch (e) {
-							done(e);
-						}
-					});
-				} catch (e) {
-					done(e);
-				}
-			});
-
-			req.on('error', function (err) {
-				done(err);
-			});
-
-			req.end();
-		});
-
-		it('a request with a nonexistent id the server should response a message', function (done) {
-			var req = http.request({
-				host: '127.0.0.1',
-				port: '8010',
-				path: '/messages/9999',
-				method: 'get'
-			}, function (res) {
-				try {
-					res.should.have.status(200);
-					res.setEncoding('utf8');
-
-					res.on('data', function (chunk) {
-						try {
-							chunk.should.equal('No message with the specified key');
-							done();
-						} catch (e) {
-							done(e);
-						}
-					});
-				} catch (e) {
-					done(e);
-				}
-			});
-
-			req.on('error', function (err) {
-				done(err);
-			});
-
-			req.end();
-		});
-
-		it('a request with a mal formatted id the server should response 400 status code with a ' +
-			'"Invalid format key" message',
-			function (done) {
-				var req = http.request({
-					host: '127.0.0.1',
-					port: '8010',
-					path: '/messages/badKey',
-					method: 'get'
-				}, function (res) {
-					try {
-						res.should.have.status(400);
-						res.setEncoding('utf8');
-
-						res.on('data', function (chunk) {
-							try {
-								chunk.should.equal('Invalid format key');
-								done();
-							} catch (e) {
-								done(e);
-							}
-						});
-					} catch (e) {
-						done(e);
-					}
-				});
-
-				req.on('error', function (err) {
-					done(err);
-				});
-
-				req.end();
-			});
 	});
 
 });
