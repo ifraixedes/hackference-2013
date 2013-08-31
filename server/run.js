10e48b7802018823cc6552f543217308b549e0eb
Ivan Fraixedes <ifcdev@gmail.com>
2013-09-01 00:46:08 +0100

    Enhanced the behaviour

--- server/run.js
+++ server/run.js
@@ -1,4 +1,24 @@
 'use strict';
 
+function shutdownServerFn(httpServer) {
 
-require('./app')();
+	return function () {
+		httpServer.close();
+		process.exit(0);
+	};
+};
+
+
+require('./app')(function (err, httpServer) {
+	if (err) {
+		console.error('Application bootstrap crashed. Error details: ' + err.message);
+		return;
+	}
+
+	process.on('SIGINT', shutdownServerFn(httpServer));
+	process.on('SIGTERM', shutdownServerFn(httpServer));
+
+	console.info('Server listening');
+});
+
+
